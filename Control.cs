ad85b9e4523818e01db75bd451637dbe3d90f7f8
Sabrina Wong <sabrina.wong@berkeley.edu>
2016-09-24 13:18:13 -0700

    please work

---
+++ Control.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D move;
+
+	// Use this for initialization
+	void Start () {
+	
+		move = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector2 moving = new Vector2 (moveHorizontal, 0);
+		move.velocity = moving * speed;
+		//move.AddForce (moving * speed);
+
+
+	}
+}
