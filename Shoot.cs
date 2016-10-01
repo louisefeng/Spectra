f70e1ebae51a267513f99ea4e80e40b195318b3d
Sabrina Wong <sabrina.wong@berkeley.edu>
2016-10-01 12:11:14 -0700

    for the bullets

---
+++ Shoot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D move;
+	public static GameObject collisionItem;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Rigidbody2D> ();
+	}
+
+	public class BulletHit : MonoBehaviour {
+		void OnCollisionEnter (Collision col) {
+			if (col.gameObject.name == "Background") {
+				Destroy(col.gameObject);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 moving = new Vector2 (0, speed);
+		move.velocity = moving * speed;
+	
+	}
+}
---
+++ bulletHit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletHit : MonoBehaviour {
+
+	public GameObject collisionItem;
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.name == "Bullet") {
+			Destroy (col.gameObject);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
