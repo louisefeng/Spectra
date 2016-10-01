a4446a40a3a2c1d883d3c0548e7ef80029177bad
Sabrina Wong <sabrina.wong@berkeley.edu>
2016-10-01 12:35:48 -0700

    also for the bullets

--- Control.cs
+++ Control.cs
@@ -6,11 +6,19 @@ public class Control : MonoBehaviour {
 	public float speed;
 
 	private Rigidbody2D move;
+	public GameObject bullet;
+	public float yPosition;
+	public GameObject ship;
+	private double time;
+	private double nextTime;
+	//public Transform bulletSpawn;
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		time = 0.5;
 		move = GetComponent<Rigidbody2D> ();
+
 	}
 	
 	// Update is called once per frame
@@ -19,7 +27,11 @@ public class Control : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		Vector2 moving = new Vector2 (moveHorizontal, 0);
 		move.velocity = moving * speed;
-		//move.AddForce (moving * speed);
+
+		if (Input.GetButton ("Fire1") && Time.time > nextTime) {
+			nextTime = Time.time + time;
+			Instantiate (bullet, new Vector3 (ship.transform.position.x, yPosition, 0), new Quaternion());
+		}
 
 
 	}
