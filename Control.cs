a880530b39079addd29b7a4dc850e7a8603eee6a
Sabrina Wong <sabrina.wong@berkeley.edu>
2016-10-01 13:30:38 -0700

    can switch between ships

--- Control.cs
+++ Control.cs
@@ -9,14 +9,22 @@ public class Control : MonoBehaviour {
 	public GameObject bullet;
 	public float yPosition;
 	public GameObject ship;
-	private double time;
-	private double nextTime;
-	//public Transform bulletSpawn;
+	public double time;
+	private double bulletNextTime;
+	private double shipNextTime;
+	public Sprite sp1;
+	public Sprite sp2;
+	public Sprite sp3;
+	public Sprite sp4;
+	private int shipNum;
+	private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
 		
-		time = 0.5;
+		shipNum = 0;
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite = sp1;
 		move = GetComponent<Rigidbody2D> ();
 
 	}
@@ -28,11 +36,28 @@ public class Control : MonoBehaviour {
 		Vector2 moving = new Vector2 (moveHorizontal, 0);
 		move.velocity = moving * speed;
 
-		if (Input.GetButton ("Fire1") && Time.time > nextTime) {
-			nextTime = Time.time + time;
+		if (Input.GetButton ("Fire1") && Time.time > bulletNextTime) {
+			bulletNextTime = Time.time + time;
 			Instantiate (bullet, new Vector3 (ship.transform.position.x, yPosition, 0), new Quaternion());
 		}
 
-
+		if (Input.GetButton("colorChange") && Time.time > shipNextTime) {
+			shipNextTime = Time.time + time;
+			shipNum = (shipNum + 1) % 4;
+			switch (shipNum) {
+				case 0:
+					spriteRenderer.sprite = sp1;
+					break;
+				case 1:
+					spriteRenderer.sprite = sp2;
+					break;
+				case 2:
+					spriteRenderer.sprite = sp3;
+					break;
+				case 3:
+					spriteRenderer.sprite = sp4;
+					break;
+			}
+		}
 	}
 }
--- Shoot.cs
+++ Shoot.cs
@@ -5,9 +5,18 @@ public class Shoot : MonoBehaviour {
 	public float speed;
 	private Rigidbody2D move;
 	public static GameObject collisionItem;
+	public double time;
+	private double nextTime;
+	public Sprite sp1;
+	public Sprite sp2;
+	public Sprite sp3;
+	public Sprite sp4;
+	public int shipNum;
+	private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
 		move = GetComponent<Rigidbody2D> ();
 	}
 
@@ -25,5 +34,23 @@ public class Shoot : MonoBehaviour {
 		Vector2 moving = new Vector2 (0, speed);
 		move.velocity = moving * speed;
 	
+		if (Input.GetButton("colorChange") && Time.time > nextTime) {
+			nextTime = Time.time + time;
+			shipNum = (shipNum + 1) % 4;
+			switch (shipNum) {
+			case 0:
+				spriteRenderer.sprite = sp1;
+				break;
+			case 1:
+				spriteRenderer.sprite = sp2;
+				break;
+			case 2:
+				spriteRenderer.sprite = sp3;
+				break;
+			case 3:
+				spriteRenderer.sprite = sp4;
+				break;
+			}
+		}
 	}
 }
